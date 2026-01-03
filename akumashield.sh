44d5fb5223f8803d0309390b7183ab740e61d9ee
sandipduley <sandipp0987rahul@gmail.com>
2026-01-03 12:02:05 +0000

    ADDED: banner & color codes

---
+++ akumashield.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Bash (ANSI) color codes
+#
+RED="\e[31m"
+# GREEN="\e[32m"
+# YELLOW="\e[33m"
+# BLUE="\e[34m"
+RESET="\e[0m"
+
+echo -e "                                            
+${RED} ▗▖ ▐                ▄▄ ▐    ▝      ▝▜    ▐ ${RESET} 
+${RED} ▐▌ ▐ ▗ ▗ ▗ ▗▄▄  ▄▖ ▐▘ ▘▐▗▖ ▗▄   ▄▖  ▐   ▄▟ ${RESET} 
+${RED} ▌▐ ▐▗▘ ▐ ▐ ▐▐▐ ▝ ▐ ▝▙▄ ▐▘▐  ▐  ▐▘▐  ▐  ▐▘▜ ${RESET} 
+${RED} ▙▟ ▐▜  ▐ ▐ ▐▐▐ ▗▀▜   ▝▌▐ ▐  ▐  ▐▀▀  ▐  ▐ ▐ ${RESET} 
+${RED}▐  ▌▐ ▚ ▝▄▜ ▐▐▐ ▝▄▜ ▝▄▟▘▐ ▐ ▗▟▄ ▝▙▞  ▝▄ ▝▙█ ${RESET} 
+"
+echo "AkumaShield — not yet another one-off hardening script"
+echo "Author: Sandip Duley"

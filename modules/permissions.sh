a582eebedd8d0055e4d654e3c8b5e24e57b7f86e
sandipduley <sandipp0987rahul@gmail.com>
2026-01-04 19:28:36 +0000

    ADDED: check passwd ownership function

--- modules/permissions.sh
+++ modules/permissions.sh
@@ -7,21 +7,21 @@ YELLOW="\e[33m"
 RESET="\e[0m"
 
 check_shadow_permission() {
-	local file="/etc/shadow"
+	local shadow_file="/etc/shadow"
 	local permission
-	permission=$(stat -c "%a" "${file}")
+	permission=$(stat -c "%a" "${shadow_file}")
 
-	echo -e "\n${GREEN}[X] Checking ${file} permissions....${RESET}"
+	echo -e "\n${GREEN}[X] Checking ${shadow_file} file permissions....${RESET}"
 
 	if [[ ${permission} -eq 600 ]]; then
-		echo -e "\n${file} permissions is secure (${permission})"
+		echo -e "\n${shadow_file} file permissions is secure default (${permission})"
 	else
-		echo -e "${YELLOW}[WARN] ${file} permissions appear to be tampered with (${permission})${RESET}"
+		echo -e "${YELLOW}[WARN] ${shadow_file} file permissions appear to be tampered with (${permission})${RESET}"
 
 		chmod 600 /etc/shadow
-		permission=$(stat -c "%a" "${file}")
+		permission=$(stat -c "%a" "${shadow_file}")
 
-		echo -e "[FIXED] ${file} permissions set to (${permission})"
+		echo -e "[FIXED] ${shadow_file} file permissions set to default (${permission})"
 	fi
 }
 check_shadow_permission
@@ -29,21 +29,21 @@ check_shadow_permission
 echo "======================================================================"
 
 check_passwd_permission() {
-	local file="/etc/passwd"
+	local passwd_file="/etc/passwd"
 	local permission
-	permission=$(stat -c "%a" "${file}")
+	permission=$(stat -c "%a" "${passwd_file}")
 
-	echo -e "\n${GREEN}[X] Checking ${file} permissions....${RESET}"
+	echo -e "\n${GREEN}[X] Checking ${passwd_file} file permissions....${RESET}"
 
 	if [[ ${permission} -eq 644 ]]; then
-		echo -e "\n${file} permission is secure (${permission})"
+		echo -e "\n${passwd_file} file permission is secure default (${permission})"
 	else
-		echo -e "${YELLOW}[WARN] ${file} permissions appear to be tampered with (${permission})${RESET}"
+		echo -e "${YELLOW}[WARN] ${passwd_file} file permissions appear to be tampered with (${permission})${RESET}"
 
 		chmod 644 /etc/passwd
-		permission=$(stat -c "%a" "${file}")
+		permission=$(stat -c "%a" "${passwd_file}")
 
-		echo "[FIXED] ${file} permission set to (${permission})"
+		echo "[FIXED] ${passwd_file} file permission set to default (${permission})"
 	fi
 }
 check_passwd_permission
